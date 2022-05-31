7755804d473603d259c3b3d1e91c773efa1da21b
JollyRedGiant <64712869+JollyRedGiantHK@users.noreply.github.com>
2022-05-31 16:10:43 -0700

    Create quadratic_formula.cpp

---
+++ quadratic_formula.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  //Variables for the quadratic formula
+  double a;
+  double b;
+  double c;
+
+  //Prompting to enter the variable values
+  std::cout << "Enter a: ";
+  std::cin >> a;
+  
+  std::cout << "Enter b: ";
+  std::cin >> b;
+
+  std::cout << "Enter c: ";
+  std::cin >> c;
+  
+  //Variables for storing the root values
+  double root1;
+  double root2;
+
+  root1 = (-b + std::sqrt(b*b - 4 * a * c)) / (2 * a);
+  root2 = (-b - std::sqrt(b*b - 4 * a * c)) / (2 * a);
+
+  std::cout << "Root 1 is " << root1 << "\n";
+  std::cout << "Root 2 is " << root2 << "\n";
+
+  return 0;
+}
