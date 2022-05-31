f9addf0278058c38379c99c8dba9bfdb8e4158a3
JollyRedGiant <64712869+JollyRedGiantHK@users.noreply.github.com>
2022-05-31 16:13:49 -0700

    Create Block_Letters.cpp

---
+++ Block_Letters.cpp
@@ -0,0 +1,16 @@
+// My initials are H and K, and I play the Trumpet
+#include <iostream>
+
+int main() {
+
+  std::cout << "H    H  K   K\n";
+  std::cout << "H    H  K  K\n";
+  std::cout << "H    H  K K\n";
+  std::cout << "HHHHHH  KK\n";
+  std::cout << "H    H  KK\n";
+  std::cout << "H    H  K K\n";
+  std::cout << "H    H  K  K\n";
+  std::cout << "H    H  K   K\n";
+
+  return 0;
+}
