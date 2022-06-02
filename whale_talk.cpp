73382d69cd35a81f35b03ee686b463a7ef002d4c
Hunter Kleinman <64712869+JollyRedGiantHK@users.noreply.github.com>
2022-06-02 09:52:03 -0700

    Update whale_talk.cpp

--- whale_talk.cpp
+++ whale_talk.cpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+#include <vector>
+#include <string>
+ 
+int main() 
+{
+ 
+  // Whale, whale, whale.
+  // What have we got here?
+ 
+ std::string input = "hi, human";
+
+ /*std::cout << "This is a translater into whale talk.\n";
+ std::cout << "Please enter what you want translated: \n";
+
+ std::cin >> input;*/
+
+ std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+ std::vector<char> result;
+
+ for (int i = 0; i < input.size(); i++)
+ {
+   for(int v = 0; v < vowels.size(); v++)
+   {
+     if(input[i] == vowels[v])
+     {
+       result.push_back(input[i]); 
+     
+      if(input[i] == 'e' || input[i] == 'u')
+      {
+         result.push_back(input[i]);
+      }
+     } 
+   }
+ }
+  for (int r = 0; r < result.size(); r++)
+  {
+    std::cout << result[r]; 
+  }
+
+  std::cout << "\n";
+
+}
